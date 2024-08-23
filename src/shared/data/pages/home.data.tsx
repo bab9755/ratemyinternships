b9b8b80ee1ba2c5cd53e09f93d6b3fac3178301d
Danjari <mm12515@nyu.edu>
2024-08-22 22:42:51 -0400

    init

---
+++ src/shared/data/pages/home.data.tsx
@@ -0,0 +1,605 @@
+import {
+  IconArrowDown,
+  IconArrowsRightLeft,
+  IconBrandLinkedin,
+  IconBrandTailwind,
+  IconBrandTwitter,
+  IconBulb,
+  IconCheck,
+  IconClock,
+  IconComponents,
+  IconDownload,
+  IconListCheck,
+  IconMail,
+  IconMapPin,
+  IconPhoneCall,
+  IconRocket,
+  IconArrowNarrowRight,
+  IconBrandMeta,
+} from '@tabler/icons-react';
+import {
+  CallToActionProps,
+  ContactProps,
+  ContentProps,
+  FAQsProps,
+  FeaturesProps,
+  HeroProps,
+  PricingProps,
+  SocialProofProps,
+  StepsProps,
+  TeamProps,
+  TestimonialsProps,
+} from '../../types';
+import heroImg from '~/assets/images/hero.jpg';
+import nextJsLogo from '~/assets/images/nextjs-logo.png';
+import reactLogo from '~/assets/images/react-logo.png';
+import tailwindCssLogo from '~/assets/images/tailwind-css-logo.png';
+import typescriptLogo from '~/assets/images/typescript-logo.png';
+import cameraFrontImg from '~/assets/images/camera-front.jpg';
+import cameraBackImg from '~/assets/images/camera-back.jpg';
+import gasImg from '~/assets/images/gas.jpg';
+
+// Hero data on Home page *******************
+export const heroHome: HeroProps = {
+  title: (
+    <>
+      Get ready for <span></span> {' '}
+      <span className="sm:whitespace-nowrap">your next software engineering internship</span>
+    </>
+  ),
+  subtitle: (
+    <>
+      <span className="hidden md:inline">
+        <span className="font-semibold underline decoration-primary-600 decoration-wavy decoration-1 underline-offset-2">
+          RateMyInternship
+        </span>{' '}
+        is your one stop to know everything about your prospective internship, <em>from interviews</em> to <em>company culture</em>.
+      </span>{' '}
+      It has been been carefully design to make sure you are confident when applying, and ready when hired
+    </>
+  ),
+  callToAction: {
+    text: 'Get started',
+    href: 'https://github.com/onwidget/tailnext',
+    icon: IconArrowNarrowRight,
+    targetBlank: true,
+  },
+  callToAction2: {
+    text: 'View Demo',
+    href: '/',
+  },
+  image: {
+    src: heroImg,
+    alt: 'Hero TailNext',
+  },
+};
+
+// SocialProof data on Home page *******************
+export const socialProofHome: SocialProofProps = {
+  id: 'socialProof-on-home',
+  hasBackground: false,
+  images: [
+    {
+      link: 'https://nextjs.org/',
+      src: nextJsLogo,
+      alt: 'Meta Logo',
+    },
+    {
+      link: 'https://react.dev/',
+      src: reactLogo,
+      alt: 'Netflix Logo',
+    },
+    {
+      link: 'https://tailwindcss.com/',
+      src: tailwindCssLogo,
+      alt: 'Microsoft Logo',
+    },
+    {
+      link: 'https://www.typescriptlang.org/',
+      src: typescriptLogo,
+      alt: 'Amazon Logo',
+    },
+  ],
+};
+
+// Features data on Home page *******************
+export const featuresHome: FeaturesProps = {
+  id: 'features-on-home',
+  hasBackground: false,
+  columns: 3,
+  header: {
+    title: (
+      <>
+        What you get with <span className="whitespace-nowrap">RateMyInternship</span>
+      </>
+    ),
+    subtitle:
+      "Ever walked into a technical interview or a role without a clear idea what to expect? We do not find it fun either.",
+    tagline: 'Features',
+  },
+  items: [
+    {
+      title: 'Verified intern company reviews ',
+      description:
+        'Know what you are getting your self into before applying to a company.',
+      icon: IconBrandTailwind,
+      callToAction: {
+        text: 'Discover now',
+        href: '/',
+      },
+    },
+    {
+      title: 'Technical interview patterns',
+      description:
+        'You cannot study everything. Be familiar with the most frequent questions different companies ask',
+      icon: IconComponents,
+      callToAction: {
+        text: 'Discover now',
+        href: '/',
+      },
+    },
+    {
+      title: 'Best Practices',
+      description:
+        'Get tips on your entire internship recruitment process.',
+      icon: IconListCheck,
+      callToAction: {
+        text: 'Discover now',
+        href: '/',
+      },
+    },
+    {
+      title: 'Grow your network',
+      description:
+        'Your network is your net-worth. Surround yourself with talented engineers that worked at your dream company.',
+      icon: IconRocket,
+      callToAction: {
+        text: 'Discover now',
+        href: '/',
+      },
+    },
+  ],
+};
+
+// Content data on Home page *******************
+export const contentHomeOne: ContentProps = {
+  id: 'contentOne-on-home-one',
+  hasBackground: true,
+  header: {
+    title: 'Aliquip definiebas ad est',
+    subtitle: 'Quando cetero his ne, eum admodum sapientem ut',
+    tagline: 'Content',
+  },
+  content:
+    'Ne dicta praesent ocurreret has, diam theophrastus at pro. Eos etiam regione ut, persius eripuit quo id. Sit te euismod tacimates.',
+  items: [
+    {
+      title: 'Per ei quaeque sensibus',
+      description:
+        'Ex usu illum iudico molestie. Pro ne agam facete mediocritatem, ridens labore facete mea ei. Pro id apeirian dignissim.',
+    },
+    {
+      title: 'Cu imperdiet posidonium sed',
+      description:
+        'Amet utinam aliquando ut mea, malis admodum ocurreret nec et, elit tibique cu nec. Nec ex maluisset inciderint, ex quis.',
+    },
+    {
+      title: 'Nulla omittam sadipscing mel ne',
+      description:
+        'At sed possim oporteat probatus, justo graece ne nec, minim commodo legimus ut vix. Ut eos iudico quando soleat, nam modus.',
+    },
+  ],
+  image: {
+    src: cameraFrontImg,
+    alt: 'Colorful Image',
+  },
+  isReversed: false,
+  isAfterContent: false,
+};
+
+// Content data on Home page *******************
+export const contentHomeTwo: ContentProps = {
+  id: 'contentOne-on-home-two',
+  hasBackground: true,
+  content:
+    'Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nib dissentias, mei exerci tamquam euripidis cu.',
+  items: [
+    {
+      title: 'Per ei quaeque sensibus',
+    },
+    {
+      title: 'Cu imperdiet posidonium sed',
+    },
+    {
+      title: 'Nulla omittam sadipscing mel ne',
+    },
+    {
+      title: 'Per ei quaeque sensibus',
+    },
+    {
+      title: 'Cu imperdiet posidonium sed',
+    },
+    {
+      title: 'Nulla omittam sadipscing mel ne',
+    },
+  ],
+  image: {
+    src: cameraBackImg,
+    alt: 'Colorful Image',
+  },
+  isReversed: true,
+  isAfterContent: true,
+};
+
+// Steps data on Home page *******************
+export const stepsHome: StepsProps = {
+  id: 'steps-on-home',
+  hasBackground: false,
+  isReversed: false,
+  isImageDisplayed: true,
+  image: {
+    src: gasImg,
+    alt: 'Steps image',
+  },
+  header: {
+    title: 'How it works',
+  },
+  items: [
+    {
+      title: 'Step 1',
+      description:
+        'Sign up, and start search about your dream companies and the reviews about them',
+      icon: IconArrowDown,
+    },
+    {
+      title: 'Step 2',
+      description:
+        'Get insights and start tailoring your resume, building projects, and preparing for technical interviews given those reviews and tips',
+      icon: IconArrowDown,
+    },
+    {
+      title: 'Step 3',
+      description:
+        'Apply! Apply! Apply!',
+      icon: IconArrowDown,
+    },
+    {
+      title: 'Ready!',
+    },
+  ],
+};
+
+// Testimonials data on Home page *******************
+export const testimonialsHome: TestimonialsProps = {
+  id: 'testimonials-on-home',
+  hasBackground: true,
+  header: {
+    title: 'What our customers say about us',
+    subtitle:
+      'Do not take our word for it, listen to what people who love RateMyInternship',
+  },
+  testimonials: [
+    {
+      name: 'Tayla Kirsten',
+      job: 'SWE Intern @ Meta',
+      testimonial: `I'm impressed by the speed and performance of these templates. My website now loads in the blink of an eye, significantly enhancing my visitors' experience. Thanks to TailNext, my online business is thriving.`,
+      image: {
+        src: 'https://images.unsplash.com/photo-1619734086067-24bf8889ea7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+        alt: 'Tayla Kirsten',
+      },
+      href: '/',
+    },
+    {
+      name: 'Silver Jordan',
+      job: 'SWE Intern @ Netflix',
+      testimonial: `I had never found it so easy to customize a website. TailNext's templates are incredibly flexible, and with Tailwind CSS, I've managed to give my website the look and feel I always wanted. Highly recommended!`,
+      image: {
+        src: 'https://images.unsplash.com/photo-1565049786474-1dea82a8b995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+        alt: 'Silver Jordan',
+      },
+      href: '/',
+    },
+    {
+      name: 'Kelsey Arden',
+      job: 'Data science intern @ Datadog',
+      testimonial: `As a beginner in web development, I really needed clear guidance. Tailnext made it possible. I was able to install and customize my website seamlessly, and I'm thrilled with the results!`,
+      image: {
+        src: 'https://images.unsplash.com/photo-1659057106920-da022cfbc0cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+        alt: 'Kelsey Arden',
+      },
+      href: '/',
+    },
+    {
+      name: 'Sarah Johnson',
+      job: 'Backend intern @ Oracle',
+      testimonial: `They've not only saved me a ton of time but have also made my websites look incredibly professional. The level of detail and thought that went into designing these templates is truly impressive.`,
+      image: {
+        src: 'https://images.unsplash.com/photo-1572417884940-c24659be6068?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+        alt: 'Sarah Johnson',
+      },
+      href: '/',
+    },
+  ],
+};
+
+// FAQS data on Home page *******************
+export const faqs2Home: FAQsProps = {
+  id: 'faqsTwo-on-home',
+  hasBackground: false,
+  header: {
+    title: 'Frequently Asked Questions',
+    subtitle:
+      '',
+    tagline: 'FAQS',
+  },
+  items: [
+    {
+      title: 'What do I need to start?',
+      description: `Your desire to advance your tech career, and your email address.`,
+    },
+    {
+      title: 'How do I connect with previous interns?',
+      description: `By searching companies, you get a list of individuals that have held an internship there. You can therefore contact them right away on your networking app of choice!`,
+    },
+    {
+      title: "Do I need to pay?",
+      description: `You can get started for $1 per month. That is literally less than a train ticket to advance your career! We care about making this resource accessible to everyone. You can always feel free to upgrate if you really like our app!`,
+    },
+    {
+      title: "How can I give back?",
+      description: `Once you land that internship, make sure to pay a visit to the website and give your best insights about your experience. Everyone benefits from it a lot and it strengthens the community!`,
+    },
+  ],
+};
+
+// Pricing data on Home page *******************
+export const pricingHome: PricingProps = {
+  id: 'pricing-on-home',
+  hasBackground: true,
+  header: {
+    title: 'Prices for each plan',
+    subtitle:
+      '',
+    // tagline: 'Pricing',
+  },
+  prices: [
+    {
+      title: 'free',
+      price: 1,
+      period: 'per month',
+      items: [
+        {
+          description: '100 company searches',
+        },
+        {
+          description: 'Limited access to reviews',
+        },
+        {
+          description: '10 interview patterns',
+        },
+        {
+          description: 'Reviewer contacts',
+        },
+      ],
+      callToAction: {
+        targetBlank: true,
+        text: 'Free 7-day trial',
+        href: '/',
+      },
+      hasRibbon: false,
+    },
+    {
+      title: 'standard',
+      price: 9,
+      period: 'per month',
+      items: [
+        {
+          description: ' 500 company searches',
+        },
+        {
+          description: 'Full access to review',
+        },
+        {
+          description: '50 interview patterns',
+        },
+        {
+          description: 'Reviewer contacts',
+        },
+      ],
+      callToAction: {
+        targetBlank: true,
+        text: 'Free 15-day trial',
+        href: '/',
+      },
+      hasRibbon: true,
+      ribbonTitle: 'Popular',
+    },
+    {
+      title: 'premium',
+      price: 16,
+      period: 'per month',
+      items: [
+        {
+          description: 'Everything in standard',
+        },
+        {
+          description: 'Resume reviews and suggestions',
+        },
+        {
+          description: 'AI Cover Letter Buider',
+        },
+        {
+          description: 'Company application suggestions',
+        },
+      ],
+      callToAction: {
+        targetBlank: true,
+        text: 'Free 30-day trial',
+        href: '/',
+      },
+      hasRibbon: false,
+    },
+  ],
+};
+
+// Team data on Home page *******************
+export const teamHome: TeamProps = {
+  id: 'team-on-home',
+  hasBackground: false,
+  header: {
+    title: 'Team Members',
+    subtitle:
+      'Meet the people behind RateMyInternship!',
+    // tagline: 'Team',
+  },
+  teams: [
+    {
+      name: 'Moudjahid Moussa',
+      occupation: 'Site Reliabily Engineer',
+      image: {
+        src: 'https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+        alt: 'Cindy Belcher',
+      },
+      items: [
+        {
+          title: 'Know more on Twitter',
+          icon: IconBrandTwitter,
+          href: '#',
+        },
+        {
+          title: 'Know more on Linkedin',
+          icon: IconBrandLinkedin,
+          href: '#',
+        },
+        {
+          title: 'Contact by email',
+          icon: IconMail,
+          href: '#',
+        },
+      ],
+    },
+    {
+      name: 'Boubacar Ballo',
+      occupation: 'Backend Engineer',
+      image: {
+        src: 'https://images.unsplash.com/photo-1614583224978-f05ce51ef5fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2172&q=80',
+        alt: 'Toby Foster',
+      },
+      items: [
+        {
+          title: 'Know more on Twitter',
+          icon: IconBrandTwitter,
+          href: '#',
+        },
+        {
+          title: 'Know more on Linkedin',
+          icon: IconBrandLinkedin,
+          href: '#',
+        },
+        {
+          title: 'Contact by email',
+          icon: IconMail,
+          href: '#',
+        },
+      ],
+    },
+
+  ],
+};
+
+// Contact data on Home page *******************
+export const contactHome: ContactProps = {
+  hasBackground: true,
+  header: {
+    title: 'Get in Touch',
+    subtitle: 'In hac habitasse platea dictumst',
+    tagline: 'Contact',
+  },
+  content:
+    'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis nec ipsum orci. Ut scelerisque sagittis ante, ac tincidunt sem venenatis ut.',
+  items: [
+    {
+      title: 'Our Address',
+      description: ['1230 Maecenas Street Donec Road', 'New York, EEUU'],
+      icon: IconMapPin,
+    },
+    {
+      title: 'Contact',
+      description: ['Mobile: +1 (123) 456-7890', 'Mail: tailnext@gmail.com'],
+      icon: IconPhoneCall,
+    },
+    {
+      title: 'Working hours',
+      description: ['Monday - Friday: 08:00 - 17:00', 'Saturday & Sunday: 08:00 - 12:00'],
+      icon: IconClock,
+    },
+  ],
+  form: {
+    title: 'Ready to Get Started?',
+    inputs: [
+      {
+        type: 'text',
+        name: 'name',
+        autocomplete: 'off',
+        placeholder: 'Your name',
+      },
+      {
+        type: 'email',
+        name: 'email',
+        autocomplete: 'on',
+        placeholder: 'Your email address',
+      },
+    ],
+    textarea: {
+      cols: 30,
+      rows: 5,
+      name: 'textarea',
+      placeholder: 'Write your message...',
+    },
+    btn: {
+      title: 'Send Message',
+      type: 'submit',
+    },
+  },
+};
+
+// CallToAction data *******************
+export const callToAction2Home: CallToActionProps = {
+  title: 'Next.js + Tailwind CSS',
+  subtitle:
+    'Aliquam sodales porttitor lacus ac tristique. Etiam posuere elit at leo feugiat sodales. Sed ac mauris quis sem tempor condimentum non at metus.',
+  callToAction: {
+    text: 'Get template',
+    href: 'https://github.com/onwidget/tailnext',
+    icon: IconDownload,
+  },
+  items: [
+    {
+      title: 'Get template',
+      description: 'Aliquam sodales est lectus, quis.',
+      href: 'https://github.com/onwidget/tailnext',
+    },
+    {
+      title: 'Learn more',
+      description: 'Class aptent taciti sociosqu ad litora torquent per conubia.',
+      href: '/',
+    },
+    {
+      title: 'Subscribe',
+      description: 'Morbi orci nunc, euismod ac dui id, convallis.',
+      form: {
+        icon: IconMail,
+        input: {
+          type: 'email',
+          name: 'email',
+          autocomplete: 'email',
+          placeholder: 'Enter your email address',
+        },
+        btn: {
+          title: 'Subscribe',
+          type: 'submit',
+        },
+      },
+    },
+  ],
+};
